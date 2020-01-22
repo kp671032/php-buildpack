d0048f3d76d6bdafd07a6f2e77085c62f8a1cbb9
root <root@ibnbld001222.bpc.broadcom.net>
2020-01-22 09:05:43 +0000

    reverrted to 7.2.26

--- fixtures/cake_local_deps/vendor/cakephp/cakephp/tests/bootstrap.php
+++ fixtures/cake_local_deps/vendor/cakephp/cakephp/tests/bootstrap.php
@@ -134,7 +134,7 @@ ini_set('session.gc_divisor', '1');
 
 loadPHPUnitAliases();
 
-// Fixate sessionid early on, as php7.4+
+// Fixate sessionid early on, as php7.2+
 // does not allow the sessionid to be set after stdout
 // has been written to.
 session_id('cli');
